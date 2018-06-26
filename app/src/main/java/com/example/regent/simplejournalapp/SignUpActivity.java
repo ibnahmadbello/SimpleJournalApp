8ec1787c41c9a09b718cb1793e37bd1d0ffcfaeb
IbnAhmadBello <justforlearningcode@gmail.com>
2018-06-26 21:21:41 +0100

    Sign Up

--- app/src/main/java/com/example/regent/simplejournalapp/SignUpActivity.java
+++ app/src/main/java/com/example/regent/simplejournalapp/SignUpActivity.java
@@ -1,20 +1,107 @@
 package com.example.regent.simplejournalapp;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
-public class SignUpActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener{
 
     private EditText inputEmail, inputPassword;
     private Button btnSignIn, btnSignUp, btnResetPassword;
     private ProgressBar progressBar;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        auth = FirebaseAuth.getInstance();
+
+        inputEmail = findViewById(R.id.email);
+        inputPassword = findViewById(R.id.password);
+        btnSignIn = findViewById(R.id.sign_in_button);
+        btnSignUp = findViewById(R.id.sign_up_button);
+        btnResetPassword = findViewById(R.id.reset_password_button);
+        progressBar = findViewById(R.id.progress_bar);
+
+        btnResetPassword.setOnClickListener(this);
+        btnSignIn.setOnClickListener(this);
+        btnSignUp.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        progressBar.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.reset_password_button:
+                startActivity(new Intent(this, ResetPasswordActivity.class));
+                break;
+            case R.id.sign_in_button:
+                finish();
+            case R.id.sign_up_button:
+                String email = inputEmail.getText().toString().trim();
+                String password = inputPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)){
+                    Toast.makeText(getApplicationContext(), "Enter email address!",
+                            Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)){
+                    Toast.makeText(getApplicationContext(), "Enter password!",
+                            Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (password.length() < 8){
+                    Toast.makeText(getApplicationContext(),
+                            "Password too short, enter minimum of 8 characters!",
+                            Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+                // Create user
+                auth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                Toast.makeText(SignUpActivity.this,
+                                        "createUserWithEmail:onComplete:" + task.isSuccessful(),
+                                        Toast.LENGTH_SHORT).show();
+                                progressBar.setVisibility(View.GONE);
+                                if (!task.isSuccessful()){
+                                    Toast.makeText(SignUpActivity.this,
+                                            "Authentication failed." + task.getException(),
+                                            Toast.LENGTH_SHORT).show();
+                                } else {
+                                    startActivity(new Intent(SignUpActivity.this, JournalActivity.class));
+                                    finish();
+                                }
+                            }
+                        });
+                break;
+        }
     }
 }
