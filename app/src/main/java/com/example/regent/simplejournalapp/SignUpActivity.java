307c457179c1b8ded139c57113f86602204cca6c
IbnAhmadBello <justforlearningcode@gmail.com>
2018-06-26 19:33:12 +0100

    Firebase auth dependencies

--- app/src/main/java/com/example/regent/simplejournalapp/SignUpActivity.java
+++ app/src/main/java/com/example/regent/simplejournalapp/SignUpActivity.java
@@ -2,8 +2,15 @@ package com.example.regent.simplejournalapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private EditText inputEmail, inputPassword;
+    private Button btnSignIn, btnSignUp, btnResetPassword;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
