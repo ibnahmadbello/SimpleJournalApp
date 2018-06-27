4237f1e2954982a9cf9e4c3cab91dfb76f16a95a
IbnAhmadBello <justforlearningcode@gmail.com>
2018-06-27 12:42:58 +0100

    Integrate Google Sign In and Sign Out

--- app/src/main/java/com/example/regent/simplejournalapp/JournalActivity.java
+++ app/src/main/java/com/example/regent/simplejournalapp/JournalActivity.java
@@ -1,16 +1,21 @@
 package com.example.regent.simplejournalapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class JournalActivity extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth;
+    private GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,8 +39,15 @@ public class JournalActivity extends AppCompatActivity {
             case R.id.action_sign_out:
                 if (firebaseAuth.getCurrentUser() != null){
                     firebaseAuth.signOut();
+                    mGoogleSignInClient.signOut().addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            startActivity(new Intent(JournalActivity.this, LoginActivity.class));
+                        }
+                    });
+
                 } else {
-                    startActivity(new Intent(JournalActivity.this, LoginActivity.class));
+//                    startActivity(new Intent(JournalActivity.this, JournalActivity.class));
                     finish();
                 }
         }
--- app/src/main/java/com/example/regent/simplejournalapp/LoginActivity.java
+++ app/src/main/java/com/example/regent/simplejournalapp/LoginActivity.java
@@ -2,9 +2,11 @@ package com.example.regent.simplejournalapp;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,10 +19,14 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -37,14 +43,6 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        firebaseAuth = FirebaseAuth.getInstance();
-
-        if (firebaseAuth.getCurrentUser() != null){
-            startActivity(new Intent(LoginActivity.this, JournalActivity.class));
-            finish();
-        }
-
         setContentView(R.layout.activity_login);
 
         inputEmail = findViewById(R.id.email);
@@ -63,6 +61,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         signInButton.setOnClickListener(this);
 
         GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
 
@@ -138,5 +137,53 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         } else {
             startActivity(new Intent(LoginActivity.this, LoginActivity.class));
         }
+
+        if (firebaseAuth.getCurrentUser() != null){
+            startActivity(new Intent(LoginActivity.this, JournalActivity.class));
+            finish();
+        }
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e){
+                Log.d(TAG, "Google sign in failed", e);
+            }
+        }
+
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct){
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        AuthCredential authCredential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        firebaseAuth.signInWithCredential(authCredential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                            startActivity(new Intent(LoginActivity.this, JournalActivity.class));
+                        } else {
+                            Log.d(TAG, "signInWithCredential:failure", task.getException());
+                            Snackbar.make(findViewById(R.id.login_activity), "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
+                            startActivity(new Intent(LoginActivity.this, LoginActivity.class));
+                        }
+
+                        progressBar.setVisibility(View.GONE);
+                    }
+                });
+    }
+
+
 }
