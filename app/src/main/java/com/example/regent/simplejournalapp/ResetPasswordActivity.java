237601ee5b8ad8f5311b27697efbf95f7c2669cb
IbnAhmadBello <justforlearningcode@gmail.com>
2018-06-27 08:01:54 +0100

    Reset Password

--- app/src/main/java/com/example/regent/simplejournalapp/ResetPasswordActivity.java
+++ app/src/main/java/com/example/regent/simplejournalapp/ResetPasswordActivity.java
@@ -1,13 +1,94 @@
 package com.example.regent.simplejournalapp;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
-public class ResetPasswordActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ResetPasswordActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText inputEmail;
+    private Button resetButton, backButton;
+    private ProgressBar progressBar;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reset_password);
+
+        inputEmail = findViewById(R.id.email);
+        resetButton = findViewById(R.id.reset_password_button);
+        backButton = findViewById(R.id.back_button);
+        progressBar = findViewById(R.id.progress_bar);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        backButton.setOnClickListener(this);
+        resetButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.back_button:
+                finish();
+            case R.id.reset_password_button:
+                String email = inputEmail.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)){
+                    Toast.makeText(getApplicationContext(), "Enter a Registered email",
+                            Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+                firebaseAuth.sendPasswordResetEmail(email)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()){
+                                    Toast.makeText(ResetPasswordActivity.this,
+                                            "We have sent you instructions to reset your password!",
+                                            Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(ResetPasswordActivity.this,
+                                            "Failed to send reset email!",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+
+                                progressBar.setVisibility(View.GONE);
+                            }
+                        });
+        }
     }
 }
+
+
+
+/*
+auth.signOut();
+
+// this listener will be called when there is change in firebase user session
+        FirebaseAuth.AuthStateListener authListener = new FirebaseAuth.AuthStateListener() {
+@Override
+public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if (user == null) {
+        // user auth state is changed - user is null
+        // launch login activity
+        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        finish();
+        }
+        }
+        };*/
