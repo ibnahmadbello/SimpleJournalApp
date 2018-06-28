108b3e13e8f2cdb13b3df0b1545750f82197ce62
IbnAhmadBello <justforlearningcode@gmail.com>
2018-06-28 15:33:25 +0100

    JournalAdapter

--- app/src/main/java/com/example/regent/simplejournalapp/JournalActivity.java
+++ app/src/main/java/com/example/regent/simplejournalapp/JournalActivity.java
@@ -1,21 +1,38 @@
 package com.example.regent.simplejournalapp;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.regent.simplejournalapp.database.model.Journal;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class JournalActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class JournalActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private List<Journal> journalList = new ArrayList<>();
     private FirebaseAuth firebaseAuth;
     private GoogleSignInClient mGoogleSignInClient;
+    private FloatingActionButton floatingActionButton;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +41,32 @@ public class JournalActivity extends AppCompatActivity {
 
         firebaseAuth = FirebaseAuth.getInstance();
 
+        floatingActionButton = findViewById(R.id.fab);
+        floatingActionButton.setOnClickListener(this);
+
+    }
+
+    /**
+     * Inserting new journal in db
+     * and refreshing the list
+     */
+    private void createJournal(String journal) {
+        // inserting journal in db and getting
+        // newly inserted journal id
+        long id = db.insertNote(journal);
+
+        // get the newly inserted note from db
+        Note n = db.getNote(id);
+
+        if (n != null) {
+            // adding new note to array list at 0 position
+            notesList.add(0, n);
+
+            // refreshing the list
+            mAdapter.notifyDataSetChanged();
+
+            toggleEmptyNotes();
+        }
     }
 
     @Override
@@ -53,4 +96,75 @@ public class JournalActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.fab:
+                showJournalDialog(false, null, -1);
+                return;
+        }
+    }
+
+    /**
+     * Shows alert dialog with EditText options to enter / edit
+     * a journal.
+     * when shouldUpdate=true, it automatically displays old note and changes the
+     * button text to UPDATE
+     */
+    private void showJournalDialog(final boolean shouldUpdate, final Journal journal, final int position) {
+        LayoutInflater layoutInflaterAndroid = LayoutInflater.from(getApplicationContext());
+        View view = layoutInflaterAndroid.inflate(R.layout.journal_dialog, null);
+
+        AlertDialog.Builder alertDialogBuilderUserInput = new AlertDialog.Builder(JournalActivity.this);
+        alertDialogBuilderUserInput.setView(view);
+
+        final EditText inputJournal = view.findViewById(R.id.journal_detail);
+        TextView dialogTitle = view.findViewById(R.id.journal_title);
+        dialogTitle.setText(!shouldUpdate ? getString(R.string.new_journal_title) : getString(R.string.update_journal_title));
+
+        if (shouldUpdate && journal != null) {
+            inputJournal.setText(journal.getJournal());
+        }
+        alertDialogBuilderUserInput
+                .setCancelable(false)
+                .setPositiveButton(shouldUpdate ? "update" : "save", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialogBox, int id) {
+
+                    }
+                })
+                .setNegativeButton("cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialogBox, int id) {
+                                dialogBox.cancel();
+                            }
+                        });
+
+        final AlertDialog alertDialog = alertDialogBuilderUserInput.create();
+        alertDialog.show();
+
+        alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Show toast message when no text is entered
+                if (TextUtils.isEmpty(inputJournal.getText().toString())) {
+                    Toast.makeText(JournalActivity.this, "Enter note!", Toast.LENGTH_SHORT).show();
+                    return;
+                } else {
+                    alertDialog.dismiss();
+                }
+
+                // check if user updating note
+                if (shouldUpdate && journal != null) {
+                    // update note by it's id
+                    updateJournal(inputJournal.getText().toString(), position);
+                } else {
+                    // create new note
+                    createJournal(inputJournal.getText().toString());
+                }
+            }
+        });
+    }
+
+
 }
--- app/src/main/java/com/example/regent/simplejournalapp/utils/JournalAdapter.java
+++ app/src/main/java/com/example/regent/simplejournalapp/utils/JournalAdapter.java
@@ -1,4 +1,88 @@
 package com.example.regent.simplejournalapp.utils;
 
-public class JournalAdapter {
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.regent.simplejournalapp.R;
+import com.example.regent.simplejournalapp.database.model.Journal;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+public class JournalAdapter extends RecyclerView.Adapter<JournalAdapter.JournalViewHolder>{
+
+    private Context context;
+    private List<Journal> journalList;
+
+    public JournalAdapter(Context context, List<Journal> journalList){
+        this.context = context;
+        this.journalList = journalList;
+    }
+
+    public class JournalViewHolder extends RecyclerView.ViewHolder{
+        public TextView dot;
+        public TextView timestamp;
+        public TextView journaldetail;
+
+        public JournalViewHolder(View view){
+            super(view);
+            dot = view.findViewById(R.id.dot);
+            timestamp = view.findViewById(R.id.timestamp);
+            journaldetail = view.findViewById(R.id.journal_detail);
+        }
+
+    }
+
+    @Override
+    public JournalViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.journal_item_row, parent, false);
+        return new JournalViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(JournalViewHolder holder, int position) {
+
+        Journal journal = journalList.get(position);
+
+        holder.journaldetail.setText(journal.getJournal());
+
+        // Displaying dot from HTML character code
+        holder.dot.setText(Html.fromHtml("&#8226;"));
+
+        // Formatting and displaying timestamp
+        holder.timestamp.setText(formatDate(journal.getTimestamp()));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return journalList.size();
+    }
+
+    /**
+     * Formatting timestamp to `MMM d` format
+     * Input: 2018-06-28 15:32:42
+     * Output: June 28
+     */
+    private String formatDate(String dateStr) {
+        try {
+            SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            Date date = fmt.parse(dateStr);
+            SimpleDateFormat fmtOut = new SimpleDateFormat("MMM d");
+            return fmtOut.format(date);
+        } catch (ParseException e) {
+
+        }
+
+        return "";
+    }
+
 }
