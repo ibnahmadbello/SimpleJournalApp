f6241247ab0a57877d5b4217b06262c28116a1f2
IbnAhmadBello <justforlearningcode@gmail.com>
2018-06-29 21:14:01 +0100

    JournalActivity working from login screen

--- app/src/main/java/com/example/regent/simplejournalapp/SignUpActivity.java
+++ app/src/main/java/com/example/regent/simplejournalapp/SignUpActivity.java
@@ -12,13 +12,11 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+
 
 public class SignUpActivity extends AppCompatActivity implements View.OnClickListener{
 
